8ce2f3e29f8d6268f131348be64c3d289cf10244
谷幸东 <Paddygu@feelwx.com>
2018-06-12 17:04:41 +0800

    edit

--- main/test/struct2json.go
+++ main/test/struct2json.go
@@ -13,9 +13,10 @@ type Student struct {
 
 func main()  {
 
+	// 结构体可以少参数
 	student := Student{Name:"小明",Age:10}
 
 	studentByte,_ := json.Marshal(student)
-	
+
 	fmt.Println(string(studentByte))
 }
